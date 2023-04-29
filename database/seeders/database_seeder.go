8db04a29cd582cd96b86a760c7e8e1b2922ca559
Mirza Hilmi <exquisitemirza@student.ub.ac.id>
2023-04-29 17:43:12 +0700

    refactor: better error handling and overal code structure

--- database/seeders/database_seeder.go
+++ database/seeders/database_seeder.go
@@ -1,7 +1,14 @@
 package seeders
 
-import "gorm.io/gorm"
+import (
+	"github.com/MirzaHilmi/SBD-GO/models"
+	"gorm.io/gorm"
+)
 
-func Seed(db gorm.DB, model []interface{}) {
-	db.Create(&model)
+func Seed(db *gorm.DB, model *[]models.Address) error {
+	if err := db.Create(&model); err.Error != nil {
+		return err.Error
+	}
+
+	return nil
 }
--- database/seeders/database_seeder_test.go
+++ database/seeders/database_seeder_test.go
@@ -1,9 +1,6 @@
-package seeders_test
+package seeders
 
 import (
-	"fmt"
-	"log"
-	"os"
 	"testing"
 
 	"github.com/MirzaHilmi/SBD-GO/config"
@@ -11,20 +8,19 @@ import (
 	"github.com/MirzaHilmi/SBD-GO/models"
 	"github.com/go-faker/faker/v4"
 	"github.com/joho/godotenv"
-	"github.com/oklog/ulid/v2"
 )
 
 func TestSeed(t *testing.T) {
 	// Load env file
 	err := godotenv.Load("../../.env")
 	if err != nil {
-		log.Fatalln("FAILED to load Environtment Variables")
+		t.Fatal("FAILED to load Environtment Variables")
 	}
 
 	// Initialize database connection
 	db, err := config.GetDB()
 	if err != nil {
-		log.Fatalln("FAILED to initialize database connection")
+		t.Fatal("FAILED to initialize database connection")
 	}
 
 	fakers.LoadCustomFaker()
@@ -32,21 +28,25 @@ func TestSeed(t *testing.T) {
 	var batch []models.Address
 	var tempAddress models.Address
 
-	for i := 0; i < 2_000; i++ {
+	for i := 0; i < 5_000; i++ {
 		if err := faker.FakeData(&tempAddress); err != nil {
-			fmt.Fprintf(os.Stderr, "error: %v\n", err)
-			os.Exit(1)
+			t.Fatal("ERROR:", err.Error())
 		}
 
-		tempAddress.ID = ulid.Make().String()
 		tempAddress.Agency.AddressID = tempAddress.ID
 
-		if tempAddress.Agency.Gender == "Prefer to skip" {
-			tempAddress.Agency.Gender = "FEMALE"
+		if !isValidGender(tempAddress.Agency.Gender) {
+			tempAddress.Agency.Gender = "MALE"
 		}
 
 		batch = append(batch, tempAddress)
 	}
 
-	db.Create(&batch)
+	if err := Seed(db, &batch); err != nil {
+		t.Fatal("ERROR:", err.Error())
+	}
+}
+
+func isValidGender(gender string) bool {
+	return gender == "MALE" || gender == "FEMALE"
 }
